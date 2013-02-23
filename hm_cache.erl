60ce8b065c20ec1018224574632c49a1ac88b0d0
David.Gao <david@747.cn>
2013-02-23 19:14:19 +0800

    rename functions

--- hm_cache.erl
+++ hm_cache.erl
@@ -1,12 +1,12 @@
 -module(hm_cache).
--export([start/1]).
+-export([start_link/0,start_link/1]).
 -export([stop/0]).
 -export([get/3, set/4,set/5, delete/3]).
 
-start() ->
+start_link() ->
     hm_cache_sup:start_link([]).
 
-start(Options) ->
+start_link(Options) ->
     hm_cache_sup:start_link(Options).
 
 stop()->
